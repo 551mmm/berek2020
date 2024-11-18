fd7ea89547d4a73369a10c5483e8b39dfec1dcd7
Papp.Marcell <Papp.Marcell@28-DIAK8>
2024-11-18 12:08:49 +0100

    Add project files.

---
+++ 241118/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _241118
+{
+    internal class Class1
+    {
+        public string nev { get; set;}
+        public string neme { get; set;}
+        public string reszleg { get; set;}
+        public int belepes { get; set;}
+        public int ber { get; set; }
+
+        public Class1(string sor)
+        {
+            string[] s = sor.Split(";");
+            nev = s[0];
+            neme = s[1];
+            reszleg = s[2];
+            belepes = Convert.ToInt32(s[3]);
+            ber = Convert.ToInt32(s[4]);
+        }
+    }
+
+    
+}
---
+++ 241118/Program.cs
@@ -0,0 +1,57 @@
+﻿using _241118;
+using System.Text;
+
+List<Class1> lista = [];
+
+using StreamReader sr = new("berek20201.txt", Encoding.UTF8);
+while (!sr.EndOfStream)
+{
+    lista.Add(new(sr.ReadLine()));
+}
+
+Console.WriteLine($"3. a dolgozok száma:{lista.Count}");
+
+int ossz = 0;
+for (int i = 0; i < lista.Count; i++) 
+{
+    ossz += lista[i].ber;
+}
+Console.WriteLine($"4. Bérek átlaga:{ossz/lista.Count/1000:0.0} eFt");
+
+Console.Write($"5.feladat:Kérem a részleg nevét:");
+
+string  reszleg= Console.ReadLine();
+bool sv = false;
+int ber = 0;
+string nev = "";
+string nem = "";
+int belepes = 0;
+for (int i = 0; i< lista.Count;i++) {
+    if (lista[i].reszleg==reszleg)
+    {
+        sv = true;
+        if (ber < lista[i].ber)
+        {
+            ber = lista[i].ber;
+            nev= lista[i].nev;
+            nem = lista[i].neme;
+            belepes = lista[i].belepes;
+        }
+    }
+}
+if (sv)
+{
+    Console.WriteLine("6. feladat:");
+    Console.WriteLine($"\tnev:{nev}");
+    Console.WriteLine($"\tNem:{nem}");
+    Console.WriteLine($"\tBelepes:{belepes}");
+    Console.WriteLine($"\tBer:{ber}");
+}
+else Console.WriteLine("A megadott reszleg nem letezik a cegnel");
+
+    var xx=lista.GroupBy(s => s.reszleg);
+    Console.WriteLine($"7. Feladat :Statisztika");
+    foreach (var g in xx) 
+    {
+        Console.WriteLine($"\t{(g.Key)} - {(g.Count())} fő");
+    }
